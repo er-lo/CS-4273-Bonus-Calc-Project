852f86b261b2424a3fd9248324dc9f5ce152d82a
Erick Lopez <contactme@ericklopez.dev>
2024-11-20 10:51:26 -0600

    created function to handle button click on the calculator UI appends to the current number

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+"use client";
+import { useState } from "react";
+
 export default function Home() {
   const calcButtons = ["AC", "C", "log", "√", "7", "8", "9", "%", "4", "5", "6", "/", "1", "2", "3", "-", "±", "0", ".", "+"];
+
+  const [calculatorInput, setCalculatorInput] = useState("");
+
+  const handleButtonClick = (character: string) => {
+    // adds whatever is added into the calculator onto the "display"
+    setCalculatorInput((prev) => prev + character);
+  };
 
   return (
     <div className="bg-[#212327] min-h-screen flex flex-col items-center justify-center">
       <div className="rounded-lg p-6 w-80 pb-0">
         <div className="text-right mb-4 p-4 bg-[#212327] rounded text-3xl font-mono">
-          {"0"}
+          {calculatorInput || "0"}
         </div>
       </div>
       <div className="bg-[#27292e] rounded-t-lg p-6 w-80">
@@ -13,6 +23,7 @@ export default function Home() {
           {calcButtons.map((character) => (
             <button
               key={character}
+              onClick={() => handleButtonClick(character)}
               className="bg-zinc-700 text-white font-bold p-4 rounded-full hover:bg-zinc-600"
             >
               {character}
